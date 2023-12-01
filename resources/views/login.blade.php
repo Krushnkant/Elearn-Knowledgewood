e5239c935c9fa5e9bae1789b7f93f11f700c6100
Krushnkant Vadadoriya <39893686+Krushnkant@users.noreply.github.com>
2023-12-01 10:08:14 +0530

    Solve Login Error

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <link rel="icon" href="img/fav.png" sizes="16x16" type="image/png">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
-  <title>Knowledgewood | Login</title>
-  <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.min.css')}}">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
-  <link rel="stylesheet" href="{{asset('css/style.css')}}" type="text/css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="icon" href="img/fav.png" sizes="16x16" type="image/png">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+    <title>Knowledgewood | Login</title>
+    <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}" type="text/css">
 </head>
+
 <body class="">
     <section class="auth-page-body">
         <div class="container">
@@ -23,30 +25,32 @@
                                     <h4 class="text-center">LogIn</h4>
                                 </div>
                                 @if(session('success'))
-                                    <div class="alert alert-success">
-                                        {{ session('success') }}
-                                    </div>
+                                <div class="alert alert-success">
+                                    {{ session('success') }}
+                                </div>
                                 @endif
 
                                 @if(session('error'))
-                                    <div class="alert alert-danger">
-                                        {{ session('error') }}
-                                    </div>
+                                <div class="alert alert-danger">
+                                    {{ session('error') }}
+                                </div>
                                 @endif
 
                                 <div class="row mb-3">
                                     <div class="col-md-12">
-                                        <input type="text" name="username" class="form-control" value="{{ old('username') }}" placeholder="Username">
+                                        <input type="text" name="username" class="form-control"
+                                            value="{{ old('username') }}" placeholder="Username">
                                         @if ($errors->has('username'))
-                                            <span class="text-danger">{{ $errors->first('username') }}</span>
+                                        <span class="text-danger">{{ $errors->first('username') }}</span>
                                         @endif
                                     </div>
                                 </div>
                                 <div class="row mb-3">
                                     <div class="col-md-12">
-                                        <input type="text" name="password" class="form-control" value="{{ old('password') }}" placeholder="Password">
+                                        <input type="text" name="password" class="form-control"
+                                            value="{{ old('password') }}" placeholder="Password">
                                         @if ($errors->has('password'))
-                                            <span class="text-danger">{{ $errors->first('password') }}</span>
+                                        <span class="text-danger">{{ $errors->first('password') }}</span>
                                         @endif
                                     </div>
                                 </div>
@@ -67,9 +71,9 @@
         </div>
     </section>
 
-@include("footer")
-<script type="text/javascript">
-  $(".alert").fadeTo(3000, 500).slideUp(500, function(){
-    $(".alert").slideUp(500);
-  });
-</script>
+    @include("footer")
+    <script type="text/javascript">
+        $(".alert").fadeTo(3000, 500).slideUp(500, function () {
+            $(".alert").slideUp(500);
+        });
+    </script>
